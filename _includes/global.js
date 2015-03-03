c3a40014851466cd078f63dfd888d5756dcc044d
cheble <chase.heble@gmail.com>
2015-03-03 11:15:20 -0600

    update

---
+++ _includes/global.js
@@ -0,0 +1,12 @@
+<!-- JQuery -->
+<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+<!-- Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+<script src="js/ripples.min.js"></script>
+<script src="js/material.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $.material.init();
+    });
+</script>
